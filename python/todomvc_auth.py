007c5eb9fef572e8ae782e212ed6fc5e72efe21e
Nishant Deshpande <nishant@myralabs.com>
2017-10-19 19:26:35 -0700

    flask-restplus testing

---
+++ python/todomvc_auth.py
@@ -0,0 +1,124 @@
+from functools import wraps
+from flask import Flask
+from flask import request
+from flask_restplus import Api, Resource, fields
+from werkzeug.contrib.fixers import ProxyFix
+
+authorizations = {
+    'apikey': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'X-API-KEY'
+    }
+}
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+api = Api(app, version='1.0', title='TodoMVC API',
+          description='A simple TodoMVC API',
+          authorizations=authorizations, security='apikey')
+
+ns = api.namespace('todos', description='TODO operations')
+
+todo = api.model('Todo', {
+    'id': fields.Integer(readOnly=True, description='The task unique identifier'),
+    'task': fields.String(required=True, description='The task details')
+})
+
+
+class TodoDAO(object):
+    def __init__(self):
+        self.counter = 0
+        self.todos = []
+
+    def get(self, id):
+        for todo in self.todos:
+            if todo['id'] == id:
+                return todo
+        api.abort(404, "Todo {} doesn't exist".format(id))
+
+    def create(self, data):
+        todo = data
+        todo['id'] = self.counter = self.counter + 1
+        self.todos.append(todo)
+        return todo
+
+    def update(self, id, data):
+        todo = self.get(id)
+        todo.update(data)
+        return todo
+
+    def delete(self, id):
+        todo = self.get(id)
+        self.todos.remove(todo)
+
+
+DAO = TodoDAO()
+DAO.create({'task': 'Build an API'})
+DAO.create({'task': '?????'})
+DAO.create({'task': 'profit!'})
+
+def isValidAPIKeyAgentRequest(request):
+    account_id = request.headers.get("X-ACCOUNT-ID")
+    account_secret = request.headers.get("X-ACCOUNT-SECRET")
+    return (account_id and account_secret and account_id == account_secret)
+
+def require_api_key(view_function):
+    @wraps(view_function)
+    # The new, post-decoration function. Note *args and **kwargs here.
+    def decorated_function(*args, **kwargs):
+        #if request.args.get('key') and request.args.get('key') == APPKEY_HERE:
+        if isValidAPIKeyAgentRequest(api.request):
+            return view_function(*args, **kwargs)
+        else:
+            response = Response(response="Invalid API credentials", status=401)
+            return response
+    return decorated_function
+
+
+@ns.route('/')
+class TodoList(Resource):
+    '''Shows a list of all todos, and lets you POST to add new tasks'''
+    @ns.doc('list_todos')
+    @ns.marshal_list_with(todo)
+    def get(self):
+        '''List all tasks'''
+        print request
+        return DAO.todos
+
+    @ns.doc('create_todo')
+    @ns.expect(todo)
+    @ns.marshal_with(todo, code=201)
+    def post(self):
+        '''Create a new task'''
+        return DAO.create(api.payload), 201
+
+
+@ns.route('/<int:id>')
+@ns.response(404, 'Todo not found')
+@ns.param('id', 'The task identifier')
+class Todo(Resource):
+    '''Show a single todo item and lets you delete them'''
+    @ns.doc('get_todo')
+    @ns.marshal_with(todo)
+    def get(self, id):
+        '''Fetch a given resource'''
+        print api
+        return DAO.get(id)
+
+    @ns.doc('delete_todo')
+    @ns.response(204, 'Todo deleted')
+    def delete(self, id):
+        '''Delete a task given its identifier'''
+        DAO.delete(id)
+        return '', 204
+
+    @ns.expect(todo)
+    @ns.marshal_with(todo)
+    def put(self, id):
+        '''Update a task given its identifier'''
+        return DAO.update(id, api.payload)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
